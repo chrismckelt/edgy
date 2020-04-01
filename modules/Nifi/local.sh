258777025c16f693c677e7b6a5e80e1975f4bfa6
Chris McKelt <chris@mckelt.com>
2020-04-01 08:57:22 +0800

    nifi docker file - copy certs for nifi on startup

--- modules/Nifi/local.sh
+++ modules/Nifi/local.sh
@@ -1,16 +1,16 @@
 
-## start entry point
-cd  ${NIFI_HOME}
-cd ../scripts
-bash ./start.sh
-sleep 60
 
 ## LOCAL SIMULATOR
-
-# setup nifi
 cd /config/local/nifi
 cp -R * /opt/nifi/nifi-current/
 keytool -import -noprompt -alias iot-edge1 -file /config/local/certs/edge-device-ca/cert/edge-device-ca-root.cert.pem -keystore /usr/local/openjdk-8/lib/security/cacerts -storepass changeit
 keytool -import -noprompt -alias iot-edge2 -file /config/local/certs/edge-device-ca/cert/edge-device-ca.cert.pem -keystore /usr/local/openjdk-8/lib/security/cacerts -storepass changeit
 cd /opt/nifi/nifi-current/bin
-bash ./nifi.sh restart
+## bash ./nifi.sh restart
+
+
+## start entry point
+cd  ${NIFI_HOME}
+cd ../scripts
+bash ./start.sh
+sleep 10
